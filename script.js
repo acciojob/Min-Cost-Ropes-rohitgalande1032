498397c25511e0b88ea2bd6d2b9a733002850956
Rohit Galande <80148975+rohitgalande1032@users.noreply.github.com>
2025-01-16 21:24:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,21 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+  arr.sort((a, b) => a-b);
+  let totalCost = 0
+  while(arr.length>1) {
+    
+    let first = arr[0];
+    let second = arr[1];
+    
+    let cost = first + second;
+    
+    totalCost += cost;
+    
+    arr = [cost, ...arr.slice(2)]
+  }
   
+  return totalCost;
 }
 
 module.exports=mincost;
