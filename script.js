a7e2fded4aa4cd046c6ad6c328505ae2c13ee1e3
Rohit Galande <80148975+rohitgalande1032@users.noreply.github.com>
2025-01-16 22:57:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,21 +2,23 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  arr.sort((a, b) => a-b);
-  let totalCost = 0
-  while(arr.length>1) {
-    
-    let first = arr[0];
-    let second = arr[1];
-    
-    let cost = first + second;
-    
+  let totalCost = 0; 
+
+  while (arr.length > 1) {
+    arr.sort((a, b) => a - b);
+
+    const first = arr[0];
+    const second = arr[1];
+
+    const cost = first + second;
+
     totalCost += cost;
-    
-    arr = [cost, ...arr.slice(2)]
+
+    // Step 5: Remove the two smallest elements and add their sum back to the array
+    arr = [cost, ...arr.slice(2)]; 
   }
-  
-  return totalCost;
+
+  return totalCost; 
 }
 
 module.exports=mincost;
